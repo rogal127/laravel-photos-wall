b6744ef9b76159e9fb1f1f5d67a876fefd035f05
p.rogala <p.rogala@savicki.pl>
2021-03-07 18:57:18 +0100

    delete users

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
+use App\Models\User;
 use App\Models\Photo;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PhotoController;
-use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -46,6 +47,35 @@ Route::get('/akceptuj-zdjecia', function() {
     if (Auth::user()->name == 'admin' || Auth::user()->name == 'rogal127') {
         $photos = Photo::where('accepted', 0)->orderBy('created_at', 'DESC')->paginate(20);
         return view('accept', ['photos' => $photos]);
+    } else {
+        return redirect('/');
+    }
+    
+})->middleware(['auth']);
+
+Route::get('/usun-uzytkownika/{id}', function($id) {
+    if (Auth::user()->name == 'admin' || Auth::user()->name == 'rogal127') {
+        $user = User::find($id);
+        if (!empty($user)) {
+            foreach ($user->photos as $p) {
+                $p->delete();
+            }
+
+            $user->delete();
+        }
+
+        return redirect('usun-uzytkownikow');
+    } else {
+        return redirect('/');
+    }
+    
+})->middleware(['auth']);
+
+Route::get('/usun-uzytkownikow', function() {
+    if (Auth::user()->name == 'admin' || Auth::user()->name == 'rogal127') {
+        $users = User::all();
+
+        return view('usun-uzytkownikow', ['users' => $users]);
     } else {
         return redirect('/');
     }
